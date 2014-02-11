fb0576ba4c860d6c7a6976b01df9f47fc1a9fcd0
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2014-02-10 23:16:52 -0800

    Start migrating editor out of pomagma

---
+++ main.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var express = require('express');
+
+var app = express();
+
+app.use('/', express.static(path.join(__dirname, 'public'))); // HACK for index
+app.use('/static', express.static(path.join(__dirname, 'public')));
+
+app.get('/corpus/lines', function (req, res) {
+  res.send(500, 'Not implemented');
+});
+
+app.get('/corpus/line/:id', function (req, res) {
+  console.log('GET line ' + req.params.id);
+  res.send(500, 'Not implemented');
+});
+
+app.put('/corpus/line', function (req, res) {
+  res.send(500, 'Not implemented');
+});
+
+app.put('/corpus/line/:id', function (req, res) {
+  console.log('PUT line ' + req.params.id);
+  res.send(500, 'Not implemented');
+});
+
+app.delete('/corpus/line/:id', function (req, res) {
+  console.log('DELETE line ' + req.params.id);
+  res.send(500, 'Not implemented');
+});
+
+app.get('/corpus/validities', function (req, res) {
+  res.send(500, 'Not implemented');
+});
+
+app.listen(process.env.PORT || 34934);
