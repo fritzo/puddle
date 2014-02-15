726ddc0889422b5f71cf656bd3372fb2d0a2ea29
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2014-02-15 13:31:44 -0800

    Make test_main.js load all javascript files in lib

--- test_main.js
+++ test_main.js
@@ -1,4 +1,19 @@
-var corpus = require('./lib/corpus');
-var test = require('./lib/test');
 
+var fs = require('fs');
+var path = require('path');
+
+var requiredir = function (dir) {
+  fs.readdirSync(dir).forEach(function(stem){
+    var name = path.join(dir, stem);
+    var stat = fs.statSync(name);
+    if (stat.isFile() && stem.match(/\.js$/)) {
+      require(name);
+    } else if (stat.isDirectory()) {
+      requiredir(name);
+    }
+  });
+};
+requiredir(path.join(__dirname, 'lib'));
+
+var test = require(path.join(__dirname, 'lib', 'test'));
 test.runAll();
