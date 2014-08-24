7ba827914b6cf66292b1cb9b9cd20d62d5249ab2
yangit <yan@yan.my>
2014-08-24 17:42:13 +0400

    Gulp / Npm run fix to work together

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var gulpif = require('gulp-if');
 var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
+var gutil = require('gulp-util');
 var argv = require('yargs').argv;
 var browserify = require('gulp-browserify');
 var nodemon = require('gulp-nodemon');
@@ -14,31 +15,38 @@ var exec = require('child_process').exec;
 gulp.task('mocha', function (cb) {
   //Using exec instead of gulp-mocha plugin beacuse of issues with plugin.
   exec('mocha ./test/**/*.js --reporter spec', function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
+    gutil.log(stdout + stderr);
+
+    if (err) {
+      cb(new gutil.PluginError('mocha', {
+        showStack: false,
+        message: 'Some tests have failed'
+      }));
+    } else {
+      cb();
+    }
   });
 });
 
 gulp.task('default', function () {
-    //copy index.html
-    gulp.src('./clientSrc/index.html')
-        .pipe(gulp.dest('./public/'));
+  //copy index.html
+  gulp.src('./clientSrc/index.html')
+    .pipe(gulp.dest('./public/'));
 
-    //process LESS -> CSS
-    gulp.src('./clientSrc/main.less')
-        .pipe(less())
-        .pipe(gulp.dest('./public'));
+  //process LESS -> CSS
+  gulp.src('./clientSrc/main.less')
+    .pipe(less())
+    .pipe(gulp.dest('./public'));
 
-    //Browserify
-    gulp.src('./clientSrc/main.js')
-        .pipe(browserify({
-            insertGlobals: true,
-            exclude: ['mocha'],
-            debug: argv.dev
-        }))
-        .pipe(gulpif(!argv.dev, uglify()))
-        .pipe(gulp.dest('./public'));
+  //Browserify
+  gulp.src('./clientSrc/main.js')
+    .pipe(browserify({
+      insertGlobals: true,
+      exclude: ['mocha'],
+      debug: argv.dev
+    }))
+    .pipe(gulpif(!argv.dev, uglify()))
+    .pipe(gulp.dest('./public'));
 });
 
 gulp.task('lint', function (cb) {
@@ -53,37 +61,32 @@ gulp.task('lint', function (cb) {
     .pipe(jshint.reporter('fail'));
 });
 
-// TODO shall probably be refactored into one function with multiple events.
-var watchClient = function (tasks) {
+var watcher = function (tasks, paths) {
   return function () {
-    var watcher = gulp.watch('./clientSrc/**/*', tasks);
-    watcher.on('change', function (event) {
-      console.log(
-          'File ' + event.path + ' was ' + event.type + ', running tasks...'
-      );
-    });
-  };
-};
-var watchAll = function (tasks) {
-  return function () {
-    var watcher = gulp.watch([
-      './clientSrc/**/*',
-      './test/**/*.js',
-      './*.js',
-      './lib/**/*.js'
-    ], tasks);
-    watcher.on('change', function (event) {
-      console.log(
-          'File ' + event.path + ' was ' + event.type + ', running tasks...'
-      );
-    });
+    gulp.start(tasks);
+    var watcher = gulp.watch(paths, tasks);
+    watcher
+      .on('change', function (event) {
+        console.log(
+            'File ' + event.path + ' was ' + event.type + ', running tasks...'
+        );
+      }).on('error', function swallowError() {
+        this.emit('end');
+      });
   };
 };
 
-gulp.task('watchLint', ['lint'], watchAll(['lint']));
-gulp.task('watchMocha', ['mocha'], watchAll(['mocha']));
 
-gulp.task('watch', ['default'], watchClient(['default']));
+gulp.task('watchLint', watcher(['lint'], [
+  './clientSrc/**/*.js',
+  './test/**/*.js',
+  './*.js',
+  './lib/**/*.js'
+]));
+
+gulp.task('watchMocha', watcher(['mocha'], ['./test/**/*.js']));
+
+gulp.task('watch', watcher(['default'],['./clientSrc/**/*']));
 
 gulp.task('serve', ['default'], function () {
   if (argv.dev === 'client') {
