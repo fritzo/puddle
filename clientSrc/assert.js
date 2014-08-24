7a0aae9f681a134e77c2a0a6d4506d49ca52df6f
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2014-08-23 23:30:56 -0700

    Remove unused assertion code

--- clientSrc/assert.js
+++ clientSrc/assert.js
@@ -19,44 +19,12 @@ var assert = function (condition, message) {
 
 assert.Exception = AssertException;
 
+// This is better than node's builtin assert comparison
 assert.equal = function (actual, expected, message) {
     assert(_.isEqual(actual, expected),
         (message || '') +
         '\n  actual = ' + JSON.stringify(actual) +
         '\n  expected = ' + JSON.stringify(expected));
-};
-
-assert.forward = function (fwd, pairs) {
-    pairs.forEach(function (pair, lineno) {
-        try {
-            assert.equal(fwd(pair[0]), pair[1]);
-        } catch (e) {
-            throw new AssertException(
-                'forward example ' + (1 + lineno) + ', ' + e);
-        }
-    });
-};
-
-assert.backward = function (bwd, pairs) {
-    pairs.forEach(function (pair, lineno) {
-        try {
-            assert.equal(bwd(pair[1]), pair[0]);
-        } catch (e) {
-            throw new AssertException(
-                'backward example ' + (1 + lineno) + ', ' + e);
-        }
-    });
-};
-
-assert.inverses = function (fwd, bwd, items) {
-    items.forEach(function (item, lineno) {
-        try {
-            assert.equal(bwd(fwd(item)), item);
-        } catch (e) {
-            throw new AssertException(
-                'inverses example ' + (1 + lineno) + ', ' + e);
-        }
-    });
 };
 
 module.exports = assert;
