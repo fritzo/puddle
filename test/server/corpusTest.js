250808d045354c417472bb28f66b8449067da209
yangit <yan@yan.my>
2014-08-24 19:29:59 +0400

    Tests rewritten for corpus-server with mocha, sinon, rewire

--- test/server/corpusTest.js
+++ test/server/corpusTest.js
@@ -1,24 +1,61 @@
-var assert = require("assert");
+'use strict';
+var assert = require('assert');
+var sinon = require('sinon');
+var rewire = require('rewire');
+
+//Note that below it is 'rewire' not 'require' !
+var corpus = rewire('../../lib/corpus');
+
 
 describe('Server', function () {
   describe('Corpus', function () {
-    it('corpus.loadStatement, corpus.dumpStatement', function () {
-      var examples = [
-        ['ASSERT I', {'name': null, 'code': 'I'}],
-        [
-          'DEFINE VAR util.box APP C I',
-          {'name': 'util.box', 'code': 'APP C I'}
-        ]
-      ];
-      examples.forEach(function (pair) {
-        assert.deepEqual(loadStatement(pair[0]), pair[1]);
-        assert.deepEqual(dumpStatement(pair[1]), pair[0]);
-      });
+    var corpusFile = [
+      '# this file is managed by corpus.js',
+      'ASSERT EQUAL APP APP C APP APP C VAR util.pair BOT VAR util.join I',
+      'ASSERT EQUAL APP APP C APP VAR util.pair BOT VAR util.join I']
+      .join('\n');
+
+    it('initialised with empty array', function () {
+      assert.equal(corpus.findAll().length, 0);
     });
 
-    it('should return -1 when the value is not present', function () {
-      assert(true);
-    })
+    it('.load() tries to read from file', function () {
+      var spy = sinon.stub().returns(corpusFile);
+      var revert = corpus.__set__('fs', {readFileSync: spy});
 
-  })
+      corpus.load();
+      assert(spy.calledOnce);
+
+      revert();
+    });
+
+    it('After .load(), findAll() returns right amount of lines', function () {
+      assert.equal(corpus.findAll().length, 2);
+    });
+
+    describe('.dump()', function () {
+      var writeSpy = sinon.stub();
+      var renameSpy = sinon.stub();
+      var revert = corpus.__set__('fs', {
+        writeFileSync: writeSpy,
+        renameSync: renameSpy
+      });
+      var lines;
+
+      before(function () {
+        lines = corpus.dump();
+        revert();
+      });
+
+      it('writes to the file and moves it', function () {
+        assert(writeSpy.calledOnce);
+        assert(renameSpy.calledOnce);
+      });
+
+      it('writes correct data to the file', function () {
+        assert.equal(lines, corpusFile);
+      });
+
+    });
+  });
 });
