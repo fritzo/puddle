0be7219dea1a11e7c4702ed09d4ea129b4269879
yangit <yan@yan.my>
2014-09-10 19:25:09 +0800

    puddle-editor monkey patch

--- puddle-editor/source/lib/corpus.js
+++ puddle-editor/source/lib/corpus.js
@@ -7,8 +7,10 @@
 
 var ajax = require('jquery').ajax;
 var _ = require('underscore');
+var syntax = require('./puddle-syntax-old');
 var tokens = require('./puddle-syntax-old').tokens;
 var assert = require('./assert');
+var assertNode = require('assert');
 var log = require('debug')('puddle:editor:corpus');
 var io = require('socket.io-client');
 var hub = require('puddle-hub').client(io);
@@ -134,14 +136,35 @@ var state = (function () {
         state.ready.set();
     };
 
-//    hub.on('reset', function (state) {
-//        var newData = [];
-//        _.each(state, function (code, id) {
-//            newData.push({id:id,});
-//        });
-//
-//        loadAll(data.data);
-//    });
+    hub.on('reset', function (state) {
+        var loadStatement = (function () {
+            var switch_ = {
+                'ASSERT': function (body) {
+                    return {'name': null, 'code': body};
+                },
+                'DEFINE': function (body) {
+                    var varName = body.split(' ', 2);
+                    assertNode.deepEqual(varName[0], 'VAR');
+                    var name = varName[1];
+                    var code = body.slice(4 + name.length + 1);
+                    return {'name': name, 'code': code};
+                }
+            };
+            return function (string) {
+                var prefix = string.split(' ', 1)[0];
+                var body = string.slice(prefix.length + 1);
+                return switch_[prefix](body);
+            };
+        })();
+        var newData = [];
+        _.each(state, function (code, id) {
+            var line = loadStatement(code);
+            line.id = id;
+            newData.push(line);
+        });
+
+        loadAll(newData);
+    });
 
     var init = function () {
 //
